58528fab8adfa48b8632cd78ebf4207b8e4c75f4
Muhammad Farhan Fahrezy <113013896+farhanfahrezyy@users.noreply.github.com>
2025-08-27 15:20:07 +0700

    fix(deviceTranslog): Transaction Log Exporting By Filter & Saved Export History on Audit

--- obj/Debug/net9.0/NewPinpadApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/NewPinpadApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewPinpadApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3330ebdae4d34c81a11322cf445647bce6961e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84417e90f3054337d184a6b630c6b968b5159e48")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewPinpadApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewPinpadApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
